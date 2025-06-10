fe332df5f0bb2f7cec27ef0c67be9378f4374cbb
Fahrul redho <faruledo@gmail.com>
2025-06-10 14:08:23 +0700

    Menampilkan gambar dalam grid

--- app/src/main/java/com/fahrulredho0018/assessment03/ui/theme/screen/MainViewModel.kt
+++ app/src/main/java/com/fahrulredho0018/assessment03/ui/theme/screen/MainViewModel.kt
@@ -1,13 +1,18 @@
 package com.fahrulredho0018.assessment03.ui.theme.screen
 
 import android.util.Log
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.fahrulredho0018.assessment03.model.Hewan
 import com.fahrulredho0018.assessment03.network.HewanApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
+
+    var data = mutableStateOf(emptyList<Hewan>())
+        private set
 
     init {
         retrieveData()
@@ -16,8 +21,7 @@ class MainViewModel : ViewModel() {
     private fun retrieveData() {
         viewModelScope.launch(Dispatchers.IO) {
             try {
-                val result = HewanApi.service.getHewan()
-                Log.d("MainViewModel", "Success: $result")
+                data.value = HewanApi.service.getHewan()
             } catch (e: Exception) {
                 Log.d("MainViewModel", "Failure: ${e.message}")
             }
